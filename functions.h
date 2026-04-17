352a1d6a7f4a5c11704bcb6c978009f7b68bea49
AdrienZM <adrienzmartins@gmail.com>
2026-04-17 17:57:23 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -4,5 +4,5 @@
 void init_admin(int plateau[7][7]);
 void afficher_admin(int plateau[7][7]);
 void init_revele(char plateau_joueur[7][7]);
-void afficher_revele(char plateau_joueur[7][7]);
+void afficher_revele(char plateau_joueur[7][7], 7);
 #endif
