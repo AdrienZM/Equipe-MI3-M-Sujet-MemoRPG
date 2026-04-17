f61283911cf91a092652ae8eee97898d7aaff5f8
AdrienZM <adrienzmartins@gmail.com>
2026-04-17 17:29:25 +0200

    Update main_program.c

--- main_program.c
+++ main_program.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "functions.h"
+#include "structure.h"
 
 int main(){
-int plateau[5][5];
-return 0;
+	int plateau_admin[7][7], plateau_joueur[7][7];
+	init(plateau_admin);
+	afficher(plateau_admin);
+	
+	return 0;
 }
