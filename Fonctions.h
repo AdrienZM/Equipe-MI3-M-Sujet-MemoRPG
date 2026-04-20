d1ff21b2de5a43f4476a8323979950ad1fe29fbe
AdrienZM <adrienzmartins@gmail.com>
2026-04-20 11:07:22 +0200

    Update Fonctions.h
    Correction pour que ce qu'on a fait avant les vacances marche

--- Fonctions.h
+++ Fonctions.h
@@ -1,8 +1,11 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include "Structure.h"
 
-void init_admin(int plateau[7][7]);
-void afficher_admin(int plateau[7][7]);
-void init_revele(char plateau_joueur[7][7]);
-void afficher_revele(char plateau_joueur[7][7], 7);
+#define TAILLE 7
+
+void init_admin(int plateau[TAILLE][TAILLE]);
+void afficher_admin(int plateau[TAILLE][TAILLE]);
+void init_revele(char plateau_joueur[TAILLE][TAILLE]);
+void afficher_revele(char plateau_joueur[TAILLE][TAILLE], int taille);
 #endif
