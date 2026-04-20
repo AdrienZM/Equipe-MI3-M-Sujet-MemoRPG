d9b042c9df6d75495a25be3761303da7b887dc84
AdrienZM <adrienzmartins@gmail.com>
2026-04-20 11:26:05 +0200

    Update Programme.c

--- Programme.c
+++ Programme.c
@@ -10,10 +10,10 @@ int main(){
 	srand(time(NULL));
 	int plateau_admin[TAILLE][TAILLE];
 	Case plateau_joueur[TAILLE][TAILLE];
-	printf("----------Plateau Admin----------");
+	printf("----------Plateau Admin----------\n");
 	init_admin(plateau_admin);
 	afficher_admin(plateau_admin);
-	printf("----------Plateau Révélé----------");
+	printf("----------Plateau Révélé----------\n");
 	init_revele(plateau_joueur);
 	afficher_revele(plateau_joueur);
 	return 0;
