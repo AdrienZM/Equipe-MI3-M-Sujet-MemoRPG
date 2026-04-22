a07bccefa5ca6e7e0e725a5ac9b6940aa8d41027
AdrienZM <adrienzmartins@gmail.com>
2026-04-22 16:34:26 +0200

    Update structure.h
    Destruction structures inutiles (monstres, objet, arme)

--- structure.h
+++ structure.h
@@ -1,26 +1,20 @@
 #ifndef STRUCTURE_H
 #define STRUCTURE_H
 
-typedef struct{
-	int x, y;
+ typedef struct{
+    int x, y;
 }Coordonnees;
 
 typedef struct{
-	Coordonnees pos;
-	int type_monstre;
-}Monstre;
-
-typedef struct{
-	Coordonnees pos; 
-	int arme, arme_antique, coffre;
-	char nom[50];
+    Coordonnees pos; 
+    int arme, arme_antique, coffre;
+    char nom[50];
 }Joueur;
 
 typedef struct{
 /* Ici on crée un struct qui représente chaque case du tableaux, dedans on met ce que contient la case et si elle a été révélé par un joueur (donc il faut la retourner)*/
-
-	char* contenu;
-	char correspond;//0: caché; 1,2,3,4: les joueurs;
-	int revele;
-}Case;
+    char* contenu;
+    int correspond, revele;
+    /*caché (0) ; joueurs (1, 2, 3, 4) ; basilics (5) ; zombies (6) ; trolls (7) ; harpies (8) ; totems (9) ; coffres au trésors (10) ; portail de téléportation (11) ; armes antiques (12, 13, 14, 15)*/
+}Case; 
 #endif
