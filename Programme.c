774cc9b47e97eb9c36d7a3c4333b1f96c0fee618
AdrienZM <adrienzmartins@gmail.com>
2026-04-25 19:13:44 +0200

    Update Programme.c

--- Programme.c
+++ Programme.c
@@ -26,21 +26,32 @@ int main(){
 		if (i == 0){
 			joueurs[i].pos.x = 0;
 			joueurs[i].pos.y = 4;
+            joueurs[i].type = 1;
 		}
 		else if(i == 1){
 			joueurs[i].pos.x = 2;
 			joueurs[i].pos.y = 0;
+            joueurs[i].type = 2;
 		}
 		else if(i == 2){
 			joueurs[i].pos.x = 6;
 			joueurs[i].pos.y = 2;
+            joueurs[i].type = 3;
 		}
 		else{
 			joueurs[i].pos.x = 4;
 			joueurs[i].pos.y = 6;
+            joueurs[i].type = 4;
 		}
 	}
-    printf("---------- Plateau ----------\n");
-	afficher(plateau);
+    int i = 0, gagnant = 0;
+    while(gagnant == 0){
+        if(i >= nb_joueur)
+            i = 0;
+        tour(&joueurs[i], plateau);
+        gagnant = est_gagnant(joueurs[i]);
+        i++;
+    }
+    printf("fin");
 	return 0;
 }
