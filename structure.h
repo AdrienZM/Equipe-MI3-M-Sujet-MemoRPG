e4c7f328ddfe1076b4c9c3c2f13fef9c0e544429
AdrienZM <adrienzmartins@gmail.com>
2026-04-26 11:48:30 +0200

    Update structure.h

--- structure.h
+++ structure.h
@@ -1,20 +1,19 @@
 #ifndef STRUCTURE_H
 #define STRUCTURE_H
 
- typedef struct{
+ typedef struct {
     int x, y;
-}Coordonnees;
+} Coordonnees;
 
-typedef struct{
-    Coordonnees pos; 
+typedef struct {
+    Coordonnees pos;
     int arme, arme_antique, coffre, type;
     char nom[50];
-}Joueur;
+} Joueur;
 
-typedef struct{
-/* Ici on crée un struct qui représente chaque case du tableaux, dedans on met ce que contient la case et si elle a été révélé par un joueur (donc il faut la retourner)*/
+typedef struct {  
     char* contenu;
     int correspond, revele;
-    /*caché (0) ; joueurs (1, 2, 3, 4) ; basilics (5) ; zombies (6) ; trolls (7) ; harpies (8) ; totems (9) ; coffres au trésors (10) ; portail de téléportation (11) ; armes antiques (12, 13, 14, 15)*/
-}Case; 
+    /*joueurs (1, 2, 3, 4) ; basilics (5) ; zombies (6) ; trolls (7) ; harpies (8) ; totems (9) ; coffres au trésors (10) ; portail de téléportation (11) ; armes antiques (12, 13, 14, 15) ; revele = 0 si cachée, 1 si révélée*/
+} Case;
 #endif
