a6fe078733e09b75780d7a51777fdfeee67589ee
AdrienZM <adrienzmartins@gmail.com>
2026-04-26 11:48:00 +0200

    Update Fonctions.h

--- Fonctions.h
+++ Fonctions.h
@@ -1,12 +1,14 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-#include "Structure.h"
+#ifndef FONCTIONS_H
+#define FONCTIONS_H
 
+#include "structure.h"
 #define TAILLE 7
 
 void init(Case plateau[TAILLE][TAILLE], int nb_joueurs);
 void afficher(Case plateau[TAILLE][TAILLE]);
-void init_joueur(Joueur* j);
 int est_gagnant(Joueur j);
-void tour(Joueur* j, Case plateau[TAILLE][TAILLE]);
+void init_joueur(Joueur* j);
+void tour(Joueur *j, Case plateau[TAILLE][TAILLE]);
+void retourner(Case plateau[TAILLE][TAILLE]);
+
 #endif
