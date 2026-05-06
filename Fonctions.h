7a9ce849762a9dafe23206b29a60169e70c2c636
AdrienZM <adrienzmartins@gmail.com>
2026-05-06 14:06:13 +0200

    Update Fonctions.h

--- Fonctions.h
+++ Fonctions.h
@@ -5,10 +5,10 @@
 #define TAILLE 7
 
 void init(Case plateau[TAILLE][TAILLE], int nb_joueurs);
-void afficher(Case plateau[TAILLE][TAILLE]);
+void afficher(Case plateau[TAILLE][TAILLE], Joueur *joueurs, int nb_joueurs);
 int est_gagnant(Joueur j);
 void init_joueur(Joueur* j);
-void tour(Joueur *j, Case plateau[TAILLE][TAILLE]);
+void tour(Joueur *j, Case plateau[TAILLE][TAILLE], Joueur *joueurs, int nb_joueurs);
 void retourner(Case plateau[TAILLE][TAILLE]);
 void sauvegarder(Joueur* joueur_partie, int nb_joueur, char* nom_gagnant);
 
