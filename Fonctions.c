69467c86457b4a940318579fcc4717b80a2b7bf9
GabrielTauzin <gabriel.tauzin@etu.cyu.fr>
2026-05-12 17:52:13 +0200

    Update Fonctions.c

--- Fonctions.c
+++ Fonctions.c
@@ -384,3 +384,242 @@ void sauvegarder(Joueur* joueurs_partie, int nb_joueurs, char* nom_gagnant) {
     fclose(f);
     printf("\nHistorique mis à jour dans 'Game_History.txt'.\n");
 }
+
+//a MODIFIER 
+char* correspondance(int numero){ //on met la correspondance d'une case et ça nous renvoie ce que ça doit afficher dans le terminal
+	switch (numero){
+	case 1 : return "test" ;
+	case 2 : return "😁" ;
+	case 3 : return "😁" ;
+	case 4 : return "😁" ;
+	case 5 : return "😁" ;
+	case 6 : return "😁" ;
+	case 7 : return "😁" ;
+	case 8 : return "😁" ;
+	case 9 : return "😁" ;
+	case 10 : return "😁" ;
+	case 11 : return "😁" ;
+	case 12 : return "😁" ;
+	case 13 : return "😁" ;
+	case 14 : return "😁" ;
+	case 15 : return "😁" ;
+	case 16 : return "😁" ;
+	default :{ printf( "Erreur, non affichable");exit(1);}
+	}
+}
+
+void afficher(Case plateau[TAILLE][TAILLE], Joueur *joueurs, Joueur *perso,int nb_joueurs) {
+    /*Cette fonction permet d'afficher le plateau en prenant en compte si la case à un joueur (pas encore fait : le joueur reste statique sur la case de départ) et si elle est révélée ou non (met différentes couleurs selon les cas)*/
+    
+    if (plateau == NULL) {
+        printf("Tableau de départ = NULL (affiche)\n");
+        return;
+    }
+
+    //on enlèvera ça plus tard (c'est beaucoup plus simple de tester le programme avec le plateau des réponses)
+    printf("----------Plateau Admin----------\n");
+    for (int i = 0 ; i < TAILLE ; i++) {
+        for (int j = 0 ; j < TAILLE ; j++) {
+            printf("%3d ", plateau[i][j].correspond);
+        }
+        printf("\n");
+    }
+	
+	for(int i = 0 ; i < nb_joueurs ; i++){
+		printf("Joueur %d : %s \n", i+1, joueurs[i].nom);
+	}
+    printf("\n----------Plateau----------\n");
+    for (int i = 0 ; i < TAILLE ; i++) {
+        for (int j = 0 ; j < TAILLE ; j++) {
+        	if(perso->pos.x == i && perso->pos.y == j){
+		    	switch (perso->type){
+		    		case 1: printf("\033[1;31m[%s]\033[0m", correspondance(plateau[i][j].correspond)) ; break; 
+				case 2: printf("\033[1;32m[%s]\033[0m", correspondance(plateau[i][j].correspond)); break; 
+				case 3: printf("\033[1;33m[%s]\033[0m", correspondance(plateau[i][j].correspond)); break; 
+				case 4: printf("\033[1;34m[%s]\033[0m", correspondance(plateau[i][j].correspond)); break; 
+            	}}
+            	
+       		else if (plateau[i][j].correspond >= 1 && plateau[i][j].correspond <= 4) {
+               		switch(plateau[i][j].correspond) {
+                		case 1: printf("\033[1;31m[%s]\033[0m", correspondance(plateau[i][j].correspond)) ; break; 
+                    		case 2: printf("\033[1;32m[%s]\033[0m", correspondance(plateau[i][j].correspond)) ; break; 
+                  	 	case 3: printf("\033[1;33m[%s]\033[0m", correspondance(plateau[i][j].correspond)) ; break; 
+                   	 	case 4: printf("\033[1;34m[%s]\033[0m", correspondance(plateau[i][j].correspond)) ; break; 
+                }
+            } 
+           
+            else if (plateau[i][j].revele == 1) {
+                printf("\033[0;36m[%s]\033[0m", correspondance(plateau[i][j].correspond)); 
+            }
+            else if (plateau[i][j].contenu[0] == ' ') {
+                printf("    ");
+            }
+            else {
+                printf("\033[0;37m[  ]\033[0m");
+            }
+      
+        }
+        printf("\n");
+    }
+}
+
+
+
+int est_gagnant(Joueur j){
+    /*Fonction qui vérifie si un joueur j est gagnant*/
+    
+	if(j.coffre >= 1 && j.arme_antique == 1)
+		return 1;
+	else
+		return 0;
+}
+
+void init_joueur(Joueur* j){
+    /*Fonction qui initialise le nom, l'arme actuelle, l'arme antique et le nombre de coffre du joueur à 0*/
+    
+    if(j == NULL){
+		printf("Joueur inexistant (init_joueur)\n");
+		return;
+    }
+    
+	printf("Saisir le nom du joueur : ");
+	scanf("%49s", j->nom);
+    printf("\n");
+	j->arme = 0;
+	j->arme_antique = 0;
+	j->coffre = 0;
+}
+
+void tour(Joueur *j, Case plateau[TAILLE][TAILLE], Joueur *joueurs, int nb_joueurs){
+    /*Fonction qui fait le tour d'un joueur : affiche le plateau, demande l'arme à choisir, demande la case où aller et en fonction de la valeur de l'élément correspond de la case, vérifie et exécute les différentes options*/
+
+    if (j == NULL || plateau == NULL) {
+        printf("Joueur inexistant ou plateau = NULL (tour)\n");
+        return;
+    }
+    
+    char case_choisie;
+    int ligne, colonne, recommencer, teleportation = 0, x, y;
+    Coordonnees depart = j->pos;
+    afficher(plateau, joueurs,j, nb_joueurs);
+    printf("\nC'est au tour de %s !\n", j->nom);
+	
+    do {
+        recommencer = 0;
+        //Demande l'arme à choisir
+        do {
+            printf("Arme : Bouclier(1), Torche(2), Hache(3), Arc(4) : ");
+            if (scanf("%d", &j->arme) != 1) {
+                printf("Mauvaise saisie. Réessayer.\n");
+                while (getchar() != '\n');
+            }
+        } while (j->arme < 1 || j->arme > 4);
+
+        /*Demande, s'il n'y a pas eu de téléportation, la prochaine case où aller ; S'il y a eu une téléportation, la case où le joueur s'est téléporté est la case qui va être révélée*/
+        x = j->pos.x;
+        y = j->pos.y;
+        if (teleportation == 0) {
+            do {
+                printf("Choisir la case où aller : 'h' --> haut ; 'g' --> gauche ; 'b' --> bas ; 'd' --> droite ");
+                scanf(" %c", &case_choisie);
+                printf("\n");
+                if (case_choisie == 'h' && j->pos.x - 1 > 0)
+                    x--;
+                else if (case_choisie == 'b' && j->pos.x + 1 < 6)
+                    x++;
+                else if (case_choisie == 'g' && j->pos.y - 1 > 0)
+                    y--;
+                else if (case_choisie == 'd' && j->pos.y + 1 < 6)
+                    y++;
+                else if (case_choisie == '!'){exit(0);}
+                else
+                    printf("Mauvaise saisie. Réessayer.\n");
+                
+                if (x < 1 || x > 5 || y < 1 || y > 5)
+                    printf("Déplacement impossible : sortie du plateau.\n");
+            } while ((case_choisie != 'h' && case_choisie != 'g' && case_choisie != 'b' && case_choisie != 'd') || x < 1 || x > 5 || y < 1 || y > 5);
+        }
+        teleportation = 0;
+        j->pos.x = x;
+        j->pos.y = y;
+
+        //Vérifie que la case choisie n'est pas déjà révélée
+        if (plateau[j->pos.x][j->pos.y].revele == 1) {
+            printf("Case déjà révélée. Réessayer.\n");
+            recommencer = 1;
+            j->pos = depart;
+        }
+            
+        else {
+            //Révélation de la case
+            plateau[j->pos.x][j->pos.y].revele = 1;
+            afficher(plateau, joueurs,j, nb_joueurs);
+            if (plateau[j->pos.x][j->pos.y].correspond <= 8 && plateau[j->pos.x][j->pos.y].correspond >= 5) { //Monstres
+                if (j->arme == plateau[j->pos.x][j->pos.y].correspond - 4) { //Bonne arme
+                    printf("Bravo ! %s a battu le monstre ! Vous pouvez continuer votre chemin.\n", j->nom);
+                    recommencer = 1;
+                }
+                else { //Mauvaise arme
+                    printf("%s es mort ! Il réapparait à sa position initiale.", j->nom);
+                    j->pos = depart;
+                }
+            }
+            else if (plateau[j->pos.x][j->pos.y].correspond == 9) { //Totem
+                printf("%s a découvert un Totem de Transmutation !\n", j->nom);
+                do {
+                    printf("Quelle case cachée veux-tu échanger ? (ligne (1 à 5) colonne (1 à 5)\n");
+                    scanf("%d %d", &ligne, &colonne);
+                    if (ligne < 1 || ligne > 5 || (colonne < 1) || colonne > 5)
+                        printf("Mauvaise saisie : sortie du plateau.\n");
+                    else if (plateau[ligne][colonne].revele == 1)
+                        printf("Case déjà révélée. Mauvaise saisie.\n");
+                } while (ligne < 1 || ligne > 5 || (colonne < 1) || colonne > 5 || plateau[ligne][colonne].revele == 1);
+                //échange des deux cases
+                Case t = plateau[j->pos.x][j->pos.y];
+                plateau[j->pos.x][j->pos.y] = plateau[ligne][colonne];
+                plateau[ligne][colonne] = t;
+                j->pos = depart;
+            }
+            else if (plateau[j->pos.x][j->pos.y].correspond == 10) { //Coffre au Trésor
+                printf("Bravo ! %s a découvert un Coffre au Trésor !\n", j->nom);
+                j->coffre++;
+                recommencer = 1;
+                if (est_gagnant(*j) == 1) {
+                    printf("Bravo ! %s est gagnant !\n", j->nom);
+                    break;
+                }
+            }        
+            else if (plateau[j->pos.x][j->pos.y].correspond == 11) { //Portail de Téléportation
+                printf("Bravo ! %s a découvert un Portail de Téléportation !\n", j->nom);
+                do {
+                    printf("Sur quelle case cachée veux-tu te téléporter ? (ligne (1 à 5) colonne (1 à 5)\n");
+                    scanf("%d %d", &ligne, &colonne);
+                    if (ligne < 1 || ligne > 5 || (colonne < 1) || colonne > 5)
+                        printf("Mauvaise saisie : sortie du plateau.\n");
+                    else if (plateau[ligne][colonne].revele == 1)
+                        printf("Case déjà révélée. Mauvaise saisie.\n");
+                } while (ligne < 1 || ligne > 5 || (colonne < 1) || colonne > 5 || plateau[ligne][colonne].revele == 1);
+                
+                printf("%s se téléporter sur la case %d %d !\n", j->nom, ligne, colonne);
+                j->pos.x = ligne;
+                j->pos.y = colonne;
+                recommencer = 1;
+                teleportation = 1;
+            }
+            else if (plateau[j->pos.x][j->pos.y].correspond <= 15 && plateau[j->pos.x][j->pos.y].correspond >= 12) { //Arme Antique
+                printf("Bravo ! %s a découvert une Arme Antique !\n", j->nom);
+                if (j->type == plateau[j->pos.x][j->pos.y].correspond - 11) {
+                    printf("%s a découvert son Arme Antique !\n", j->nom);
+                    j->arme_antique++;
+                    if (est_gagnant(*j) == 1) {
+                        printf("Bravo ! %s est gagnant !\n", j->nom);
+                        break;
+                    }
+                }
+                else
+                    printf("Dommage ! %s n'a pas découvert son Arme Antique !\n", j->nom);
+                recommencer = 1;
+            }
+        }
+    } while (recommencer == 1);
+}
