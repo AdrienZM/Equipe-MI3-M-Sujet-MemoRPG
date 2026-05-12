794d3bc283b9a2e158d10afb334779c7c8665e3e
AdrienZM <adrienzmartins@gmail.com>
2026-05-12 17:43:06 +0200

    Update Fonctions.h

--- Fonctions.h
+++ Fonctions.h
@@ -8,6 +8,7 @@ void init(Case plateau[TAILLE][TAILLE], int nb_joueurs);
 void afficher(Case plateau[TAILLE][TAILLE], Joueur *joueurs, int nb_joueurs);
 int est_gagnant(Joueur j);
 void init_joueur(Joueur* j);
+int peut_jouer(Joueur *j, Case plateau[TAILLE][TAILLE]);
 void tour(Joueur *j, Case plateau[TAILLE][TAILLE], Joueur *joueurs, int nb_joueurs);
 void retourner(Case plateau[TAILLE][TAILLE]);
 void sauvegarder(Joueur* joueur_partie, int nb_joueur, char* nom_gagnant);
