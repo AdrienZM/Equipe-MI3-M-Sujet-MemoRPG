22abeae0a1cb46771c079f06a2c4020e04c0f698
AdrienZM <adrienzmartins@gmail.com>
2026-05-13 08:20:57 +0200

    Update Fonctions.h

--- Fonctions.h
+++ Fonctions.h
@@ -12,5 +12,6 @@ int peut_jouer(Joueur *j, Case plateau[TAILLE][TAILLE]);
 void tour(Joueur *j, Case plateau[TAILLE][TAILLE], Joueur *joueurs, int nb_joueurs);
 void retourner(Case plateau[TAILLE][TAILLE]);
 void sauvegarder(Joueur* joueur_partie, int nb_joueur, char* nom_gagnant);
+char* correspondance(int numero);
 
 #endif
